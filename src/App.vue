e737d2517a0a0d14d5b39dcead675c1c6bc3265b
Abdullah Altun <abdullh.altun@gmail.com>
2022-05-29 16:28:59 +0300

    slot kullanımı

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <app-memory>
+          <h1 slot="heading" >Başlık</h1>
+          <p slot="content" >Bu çok güzel bir anıdır...</p>
+        </app-memory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Memory from "./Memory";
+
+  export default {
+    components : {
+      appMemory : Memory
+    }
+  }
+</script>
