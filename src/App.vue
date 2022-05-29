f15d251e0092d1b8edfb997486364afe454e239e
Abdullah Altun <abdullh.altun@gmail.com>
2022-05-29 17:07:29 +0300

    dinamik component kullanımı eklendi

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,40 @@
   <div class="container">
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
-        <app-memory>
+        <button @click="selectedComponent= 'appMemory' " >appMemory</button>
+        <button @click="selectedComponent= 'appAuthor' ">appAuthor</button>
+        <button @click="selectedComponent= 'appNewMemory' ">appNewMemory</button>
+        <p>{{selectedComponent}}</p>
+        <keep-alive>
+        <component :is="selectedComponent" >
           <h1 slot="heading" >Başlık</h1>
           <p slot="content" >Bu çok güzel bir anıdır...</p>
-        </app-memory>
+        </component>
+        </keep-alive>
+        <!--<app-memory>
+          <h1 slot="heading" >Başlık</h1>
+          <p slot="content" >Bu çok güzel bir anıdır...</p>
+        </app-memory> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Memory from "./Memory";
+  import Memory from "./components/Memory";
+  import Author from "./components/Author";
+  import NewMemory from "./components/NewMemory";
 
   export default {
+    data : function (){
+      return {
+        selectedComponent : "appMemory"
+      }
+    },
     components : {
-      appMemory : Memory
+      appMemory : Memory,
+      appAuthor : Author,
+      appNewMemory : NewMemory
     }
   }
 </script>
